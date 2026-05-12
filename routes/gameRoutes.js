e06d9c9a404eb069f8c02b344e8ed500189cc9d5
michanoku <michael.werker@gmail.com>
2026-05-12 01:18:18 +0000

    feat: Add game routes

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const gameController = require("../controllers/gameController");
 
 router.get("/", gameController.getAllGames);
+router.get("/add", gameController.getNewGame);
+router.post("/add", gameController.postNewGame);
+router.get("/edit/:id", gameController.getEditGame);
+router.post("/edit/:id", gameController.postEditGame);
+router.post("/delete/:id", gameController.postDeleteGame);
 
 module.exports = router;
