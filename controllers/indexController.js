52c850e211b8a047eda4c58b724364b094eb445b
michanoku <michael.werker@gmail.com>
2026-05-15 04:05:12 +0000

    feat: Add index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,8 @@
-const index = (req, res) => {
-  res.render("index", { title: "Michanoku Gamestore" });
+const db = require("../db/indexQueries");
+
+const index = async (req, res) => {
+  const counts = await db.getInventory();
+  res.render("index", { title: "Michanoku Gamestore", counts: counts });
 };
 
 module.exports = {
